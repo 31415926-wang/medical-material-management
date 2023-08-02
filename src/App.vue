5752e8794caa3265b142304ab92ae810cd192671
平平 <1550642660@qq.com>
2023-08-02 14:53:37 +0800

    首次提交

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template> 
+  <div>
+   App组件
+  </div>
+  <HelloWorld msg="父传子字符串" /> 
+</template>
+
+<style scoped>
+ 
+</style>
