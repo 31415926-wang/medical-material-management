d01a6e5cbe4c833313f006763398cdbb3d762b80
平平 <1550642660@qq.com>
2023-08-02 18:29:03 +0800

    2023.8.2更新

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,26 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import {
+  Check,
+  Delete,
+  Edit,
+  Message,
+  Search,
+  Star,
+} from '@element-plus/icons-vue'
 
 defineProps<{ msg: string }>()
 
-const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-  </div>
+  <el-row>
+    <el-button :icon="Search" circle />
+    <el-button type="primary" :icon="Edit" circle />
+    <el-button type="success" :icon="Check" circle />
+    <el-button type="info" :icon="Message" circle />
+    <el-button type="warning" :icon="Star" circle />
+    <el-button type="danger" :icon="Delete" circle />
+  </el-row>
   
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,18 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
+//获取应用实例对象
+const app=createApp(App)
+
+app.use(ElementPlus, {
+    locale: zhCn
+})
+
+
+
+//挂载到节点
+app.mount('#app')
