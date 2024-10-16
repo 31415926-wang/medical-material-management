8dc6b6c4e4c85e24d270e897335ef328afe3e4f1
wain <1550642660@qq.com>
2024-10-16 18:30:15 +0800

    10.16

--- goods-manage-vue3/src/setting.ts
+++ goods-manage-vue3/src/setting.ts
@@ -1,6 +1,7 @@
+import logo from "@/assets/images/logo.png";
 
 export default {
     title: '疫情物资管理系统', //项目的标题
-    logo: '/logo.png', //项目logo设置，public会直接复制到构建结果，适合放静态文件
+    logo: logo, //项目logo设置，public会直接复制到构建结果，适合放静态文件
 }
 
