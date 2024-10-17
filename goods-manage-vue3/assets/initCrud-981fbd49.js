8b44be08520d99398b4ccd91626f3f47b965f424
31415926-wang <31415926-wang@users.noreply.github.com>
2024-10-17 03:19:10 +0000

    deploy: fef992ebc29b1fe56ee6fb4717c879d4b16dbbed

---
+++ goods-manage-vue3/assets/initCrud-981fbd49.js
@@ -0,0 +1,1 @@
+import{o as r}from"./index-5a424909.js";function d(o,t,i,n){r(()=>{o.value.hookCrudObject.crudInit(t,i,n)})}export{d as i};
--- goods-manage-vue3/assets/initCrud-9f35936f.js
+++
@@ -1,1 +0,0 @@
-import{o as r}from"./index-9f287090.js";function d(o,t,i,n){r(()=>{o.value.hookCrudObject.crudInit(t,i,n)})}export{d as i};
