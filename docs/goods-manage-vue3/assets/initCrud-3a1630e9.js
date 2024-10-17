fef992ebc29b1fe56ee6fb4717c879d4b16dbbed
wain <1550642660@qq.com>
2024-10-17 11:17:49 +0800

    10.17

---
+++ docs/goods-manage-vue3/assets/initCrud-3a1630e9.js
@@ -0,0 +1,1 @@
+import{o as r}from"./index-14e5c0a4.js";function d(o,t,i,n){r(()=>{o.value.hookCrudObject.crudInit(t,i,n)})}export{d as i};
--- goods-manage-vue3/src/utils/microApps.ts
+++ goods-manage-vue3/src/utils/microApps.ts
@@ -1,16 +1,51 @@
-import { registerMicroApps, start } from 'qiankun'
+import { registerMicroApps, start, addErrorHandler, addGlobalUncaughtErrorHandler } from 'qiankun'
 
+console.log("微前端调试1");
 
-registerMicroApps([ //注册子应用
+// registerMicroApps([ //注册子应用
+//     {
+//         name: 'son-app',
+//         entry: import.meta.env.VITE_SON_WEB_BASE_API,
+//         container: '#microAppsContainer',
+//         activeRule: '#/screen',  //路由使用哈希模式需要添加#
+//     }
+// ])
+registerMicroApps([
     {
         name: 'son-app',
         entry: import.meta.env.VITE_SON_WEB_BASE_API,
         container: '#microAppsContainer',
-        activeRule: '#/screen'  //路由使用哈希模式需要添加#
+        activeRule: '#/screen',  //路由使用哈希模式需要添加#
     }
-])
+],
+    {
+        afterMount: (app) => {
+            console.log(`子应用 "${app.name}" 加载完毕!`);
+            return Promise.resolve();
+        },
+        beforeMount: (app) => {
+            console.log(`子应用 "${app.name}" 即将挂载...`);
+            return Promise.resolve();
+        },
+        afterUnmount: (app) => {
+            console.log(`子应用 "${app.name}" 卸载完毕!`);
+            return Promise.resolve();
+        }
+    }
+)
 
-// console.log("微前端调试", window);
+
+
+addErrorHandler((err) => {
+    console.log("检测到错误", err);
+})
+addGlobalUncaughtErrorHandler((err) => {
+    console.log("检测到错误", err);
+})
+
+console.log("微前端调试2");
+
 
 start();
+
 
